05f684cbdcab85206a80df8034638c50627d1f3e
BuddhadebKoner <jeetkoner36@gmail.com>
2025-03-02 04:27:32 +0530

    production bug #1

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -281,7 +281,7 @@ export const verifyEmail = async (req, res) => {
       res.cookie("token", token, {
          httpOnly: true,
          secure: process.env.CURRENT_STATUS === "production",
-         sameSite: process.env.CURRENT_STATUS === "production" ? "none" : "strict",
+         sameSite: process.env.CURRENT_STATUS === "production" ? "none" : "lax",
          maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
